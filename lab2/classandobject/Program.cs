61e9ff09b330e58ee85337f29c13d7a6f52abbb1
hello <sudanshakya15@gmail.com>
2022-08-20 12:50:41 +0545

    Programs for lab 2 added

---
+++ lab2/classandobject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ClassAndObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+            int result = cal.CalculateSum(5, 9);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+
+    public class Calculator
+    {
+        public int CalculateSum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
